707854d98a5643abad73a0fd1896cfff507af738
Brandon Bodnar <bodnarbm@gmail.com>
2022-12-07 22:34:19 -0500

    Little cleaner tree

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,4 +1,10 @@
-use std::{collections::HashMap, fmt::Debug, io, str::FromStr};
+use std::{
+    cell::RefCell,
+    fmt::Debug,
+    io,
+    rc::{Rc, Weak},
+    str::FromStr,
+};
 
 use nom::{
     branch::alt,
@@ -28,179 +34,150 @@ fn main() -> io::Result<()> {
 type Error = String;
 type Result<T> = std::result::Result<T, Error>;
 
-#[derive(Debug)]
-enum Node {
-    File(FileNode),
-    Directory(usize),
+struct FileTree {
+    root: Rc<RefCell<Directory>>,
+    cwd: Weak<RefCell<Directory>>,
 }
 
-#[derive(Debug)]
-struct FileNode {
-    name: String,
-    size: usize,
-}
-
-#[derive(Debug)]
-struct DirectoryNode {
-    name: String,
-    contents: Vec<Node>,
-}
-
-impl DirectoryNode {
-    fn root() -> Self {
-        Self::new("/")
+impl FileTree {
+    fn new() -> Self {
+        let root = Rc::new(RefCell::new(Directory::root("/")));
+        let cwd = Rc::downgrade(&root);
+        Self { root, cwd }
+    }
+    fn change_directory(&mut self, path: &str) {
+        match path {
+            "/" => self.cwd = Rc::downgrade(&self.root),
+            ".." => {
+                let Some(strong) = self.cwd.upgrade() else {
+                    return;
+                };
+                let borrowed = strong.borrow_mut();
+                let Some(parent) = borrowed.parent.clone() else {
+                    return;
+                };
+                self.cwd = parent
+            }
+            _ => {
+                let Some(strong) = self.cwd.upgrade() else {
+                    return;
+                };
+                for child in &strong.as_ref().borrow().contents {
+                    if let FileType::Directory(child) = child {
+                        if child.as_ref().borrow().name == path {
+                            self.cwd = Rc::downgrade(child)
+                        }
+                    }
+                }
+            }
+        }
     }
 
-    fn new(name: impl Into<String>) -> Self {
+    fn mkdir(&mut self, name: impl Into<String>) {
+        let Some(cwd) = self.cwd.upgrade() else {
+            return;
+        };
+        let new_directory = Directory::new(name, &cwd);
+        cwd.borrow_mut()
+            .contents
+            .push(FileType::Directory(Rc::new(RefCell::new(new_directory))));
+    }
+
+    fn touch(&mut self, name: impl Into<String>, size: usize) {
+        let Some(cwd) = self.cwd.upgrade() else {
+            return;
+        };
+        let new_file = File::new(name, size);
+        cwd.borrow_mut().contents.push(FileType::File(new_file));
+    }
+
+    fn directories(&self) -> DirectoryIterator {
+        DirectoryIterator {
+            stack: vec![self.root.clone()],
+        }
+    }
+
+    fn total_size(&self) -> usize {
+        FileType::Directory(self.root.clone()).disk_size()
+    }
+}
+
+struct DirectoryIterator {
+    stack: Vec<Rc<RefCell<Directory>>>,
+}
+
+impl Iterator for DirectoryIterator {
+    type Item = FileType;
+    fn next(&mut self) -> Option<Self::Item> {
+        let Some(next) = self.stack.pop() else {
+            return None;
+        };
+
+        for child in &next.as_ref().borrow().contents {
+            if let FileType::Directory(child) = child {
+                self.stack.push(child.clone())
+            }
+        }
+        Some(FileType::Directory(next))
+    }
+}
+
+struct Directory {
+    name: String,
+    parent: Option<Weak<RefCell<Directory>>>,
+    contents: Vec<FileType>,
+}
+
+impl Directory {
+    fn root(name: impl Into<String>) -> Self {
         Self {
             name: name.into(),
+            parent: None,
+            contents: vec![],
+        }
+    }
+
+    fn new(name: impl Into<String>, parent: &Rc<RefCell<Directory>>) -> Self {
+        let parent = Rc::downgrade(parent);
+        Self {
+            name: name.into(),
+            parent: Some(parent),
             contents: vec![],
         }
     }
 }
 
-#[derive(Debug)]
-struct FileSystem {
-    cwd_idx: usize,
-    directories: Vec<DirectoryNode>,
-    directory_idx_to_parent_idx: HashMap<usize, usize>,
+struct File {
+    _name: String,
+    size: usize,
 }
 
-impl FileSystem {
-    fn new() -> Self {
-        let root = DirectoryNode::root();
-
+impl File {
+    fn new(name: impl Into<String>, size: usize) -> Self {
         Self {
-            cwd_idx: 0,
-            directories: vec![root],
-            directory_idx_to_parent_idx: HashMap::from([(0, 0)]),
+            _name: name.into(),
+            size,
         }
-    }
-
-    fn cwd(&self) -> &DirectoryNode {
-        &self.directories[self.cwd_idx]
-    }
-
-    fn cwd_mut(&mut self) -> &mut DirectoryNode {
-        &mut self.directories[self.cwd_idx]
-    }
-
-    fn change_directory(&mut self, path: &str) -> Result<()> {
-        match path {
-            "/" => self.cwd_idx = 0,
-            ".." => {
-                self.cwd_idx = self.directory_idx_to_parent_idx[&self.cwd_idx];
-            }
-            _ => {
-                let Some(idx) = self.child_directory_idx(path) else {
-                    return Err(format!("tried to cd to unknown directory {}", path));
-                };
-                self.cwd_idx = idx;
-            }
-        }
-        Ok(())
-    }
-
-    fn child_directory_idx(&self, path: &str) -> Option<usize> {
-        self.cwd().contents.iter().find_map(|child| {
-            let Node::Directory(directory_idx) = child else {
-                return None;
-            };
-            let directory_node = &self.directories[*directory_idx];
-            if directory_node.name == path {
-                Some(*directory_idx)
-            } else {
-                None
-            }
-        })
-    }
-
-    fn mkdir(&mut self, path: &str) -> bool {
-        let existing = self
-            .child_directory_idx(path)
-            .map(|idx| &self.directories[idx]);
-        match existing {
-            Some(_) => false,
-            None => {
-                let next_idx = self.directories.len();
-                self.directory_idx_to_parent_idx
-                    .insert(next_idx, self.cwd_idx);
-                let new_directory = DirectoryNode::new(path);
-                self.directories.push(new_directory);
-                self.cwd_mut().contents.push(Node::Directory(next_idx));
-                true
-            }
-        }
-    }
-
-    fn create_file(&mut self, name: &str, size: usize) -> bool {
-        let existing_file = self.cwd().contents.iter().find_map(|child| {
-            let Node::File(file_node) = child else {
-                return None;
-            };
-            if file_node.name == name {
-                Some(file_node)
-            } else {
-                None
-            }
-        });
-        match existing_file {
-            Some(_) => false,
-            None => {
-                let new_file_node = FileNode {
-                    name: name.to_string(),
-                    size,
-                };
-                self.cwd_mut().contents.push(Node::File(new_file_node));
-                true
-            }
-        }
-    }
-
-    fn du(&self, directory: &DirectoryNode) -> usize {
-        directory
-            .contents
-            .iter()
-            .map(|node| match node {
-                Node::Directory(d) => self.du(&self.directories[*d]),
-                Node::File(FileNode { size, .. }) => *size,
-            })
-            .sum()
-    }
-
-    fn print_directory(
-        &self,
-        directory_idx: usize,
-        indent_lvl: usize,
-        f: &mut std::fmt::Formatter<'_>,
-    ) -> std::fmt::Result {
-        let directory_node = &self.directories[directory_idx];
-        let name = &directory_node.name;
-        writeln!(f, "{:indent$}- {name} (dir)", "", indent = indent_lvl * 2)?;
-        for node in &directory_node.contents {
-            match node {
-                Node::Directory(child_idx) => {
-                    self.print_directory(*child_idx, indent_lvl + 1, f)?
-                }
-                Node::File(file_node) => {
-                    let name = &file_node.name;
-                    let size = file_node.size;
-                    writeln!(
-                        f,
-                        "{:indent$}- {name} (file, size={size})",
-                        "",
-                        indent = indent_lvl * 2 + 2
-                    )?
-                }
-            }
-        }
-        Ok(())
     }
 }
 
-impl std::fmt::Display for FileSystem {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        self.print_directory(0, 0, f)
+enum FileType {
+    File(File),
+    Directory(Rc<RefCell<Directory>>),
+}
+
+impl FileType {
+    fn disk_size(&self) -> usize {
+        match self {
+            Self::Directory(directory) => directory
+                .as_ref()
+                .borrow()
+                .contents
+                .iter()
+                .map(|c| c.disk_size())
+                .sum(),
+            Self::File(file) => file.size,
+        }
     }
 }
 
@@ -216,40 +193,36 @@ impl FromStr for Commands {
     }
 }
 
-impl TryFrom<Commands> for FileSystem {
-    type Error = String;
-    fn try_from(commands: Commands) -> std::result::Result<Self, Self::Error> {
-        let mut file_system = FileSystem::new();
+impl From<Commands> for FileTree {
+    fn from(commands: Commands) -> Self {
+        let mut file_system = FileTree::new();
 
         for command in commands.0 {
             match command {
-                Command::ChangeDirectory(directory) => file_system.change_directory(&directory)?,
+                Command::ChangeDirectory(directory) => file_system.change_directory(&directory),
                 Command::List(entries) => {
                     for entry in entries {
                         match entry {
                             ListOutputEntry::Directory(directory) => {
                                 file_system.mkdir(&directory);
                             }
-                            ListOutputEntry::File { name, size } => {
-                                file_system.create_file(&name, size);
-                            }
+                            ListOutputEntry::File { name, size } => file_system.touch(name, size),
                         }
                     }
                 }
             }
         }
-        Ok(file_system)
+        file_system
     }
 }
 
 fn part1(input: &str) -> Result<usize> {
     let commands: Commands = input.parse()?;
-    let file_system = FileSystem::try_from(commands)?;
+    let file_system = FileTree::from(commands);
 
     let total_size = file_system
-        .directories
-        .iter()
-        .map(|directory| file_system.du(directory))
+        .directories()
+        .map(|directory| directory.disk_size())
         .filter(|s| *s <= 100000)
         .sum();
     Ok(total_size)
@@ -257,18 +230,17 @@ fn part1(input: &str) -> Result<usize> {
 
 fn part2(input: &str) -> Result<usize> {
     let commands: Commands = input.parse()?;
-    let file_system = FileSystem::try_from(commands)?;
+    let file_system = FileTree::from(commands);
 
-    let total_used = file_system.du(&file_system.directories[0]);
+    let total_used = file_system.total_size();
     let total_disk_space: usize = 70000000;
     let free_space = total_disk_space.saturating_sub(total_used);
     let needed_for_patch: usize = 30000000;
     let must_free_up = needed_for_patch.saturating_sub(free_space);
 
     let space_to_free_up = file_system
-        .directories
-        .iter()
-        .map(|directory| file_system.du(directory))
+        .directories()
+        .map(|directory| directory.disk_size())
         .filter(|s| *s >= must_free_up)
         .min()
         .unwrap_or_default();

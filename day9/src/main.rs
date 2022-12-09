c94f3a5deaebf750679e9dab48f228adc6d679cb
Brandon Bodnar <bodnarbm@gmail.com>
2022-12-09 14:58:11 -0500

    Some vector cleanup

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,4 +1,10 @@
-use std::{collections::HashSet, io, str::FromStr};
+use std::{
+    collections::HashSet,
+    fmt::Debug,
+    io,
+    ops::{AddAssign, Sub},
+    str::FromStr,
+};
 
 fn main() {
     let input = io::read_to_string(io::stdin()).unwrap();
@@ -12,67 +18,89 @@ enum Error {
     InvalidMove,
 }
 
-#[derive(Debug)]
-struct Direction(i64, i64);
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct Vector2(i64, i64);
+
+impl Vector2 {
+    fn max_magnitude(&self) -> i64 {
+        self.0.abs().max(self.1.abs())
+    }
+}
+
+impl AddAssign for Vector2 {
+    fn add_assign(&mut self, rhs: Self) {
+        self.0 += rhs.0;
+        self.1 += rhs.1;
+    }
+}
+
+impl Sub for Vector2 {
+    type Output = Vector2;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self(self.0 - rhs.0, self.1 - rhs.1)
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Direction(Vector2);
 
 impl FromStr for Direction {
     type Err = Error;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
-            "U" => Ok(Direction(0, 1)),
-            "R" => Ok(Direction(1, 0)),
-            "D" => Ok(Direction(0, -1)),
-            "L" => Ok(Direction(-1, 0)),
+            "U" => Ok(Direction(Vector2(0, 1))),
+            "R" => Ok(Direction(Vector2(1, 0))),
+            "D" => Ok(Direction(Vector2(0, -1))),
+            "L" => Ok(Direction(Vector2(-1, 0))),
             _ => Err(Error::InvalidDirection),
         }
     }
 }
 
-#[derive(Debug)]
-struct Move {
-    direction: Direction,
-    spaces: u32,
-}
+struct Steps(Vec<Direction>);
 
-impl FromStr for Move {
+impl FromStr for Steps {
     type Err = Error;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let (direction, spaces) = s.split_once(' ').ok_or(Error::InvalidMove)?;
-        Ok(Move {
-            direction: direction.parse()?,
-            spaces: spaces.parse().or(Err(Error::InvalidMove))?,
-        })
+        let direction: Direction = direction.parse()?;
+        let spaces: usize = spaces.parse().or(Err(Error::InvalidMove))?;
+        Ok(Steps(vec![direction; spaces]))
     }
 }
 
-fn parse_input(input: &str) -> Vec<Move> {
-    input.lines().map(str::parse).map(Result::unwrap).collect()
+fn parse_input(input: &str) -> Vec<Direction> {
+    input
+        .lines()
+        .map(str::parse::<Steps>)
+        .map(Result::unwrap)
+        .flat_map(|s| s.0)
+        .collect()
 }
 
-fn count_tail_positions<const LENGTH: usize>(moves: Vec<Move>) -> usize {
-    let mut knots = [(0, 0); LENGTH];
+fn count_tail_positions<const LENGTH: usize>(moves: Vec<Direction>) -> usize {
+    let mut knots = [Vector2(0, 0); LENGTH];
 
     let mut tail_positions = HashSet::new();
     tail_positions.insert(knots[LENGTH - 1]);
 
-    for Move { spaces, direction } in moves {
-        for _ in 0..spaces {
-            let (head, rest) = knots.split_at_mut(1);
-            let mut prior = &mut head[0];
-            prior.0 += direction.0;
-            prior.1 += direction.1;
+    for direction in moves {
+        let (head, rest) = knots.split_at_mut(1);
+        let mut prior = &mut head[0];
+        *prior += direction.0;
 
-            for knot in rest.iter_mut() {
-                let diff = ((prior.0 - knot.0), (prior.1 - knot.1));
-                if diff.0.abs() > 1 || diff.1.abs() > 1 {
-                    knot.0 += diff.0.clamp(-1, 1);
-                    knot.1 += diff.1.clamp(-1, 1);
-                }
-                prior = knot;
+        for knot in rest.iter_mut() {
+            let delta = *prior - *knot;
+            if delta.max_magnitude() > 1 {
+                knot.0 += delta.0.clamp(-1, 1);
+                knot.1 += delta.1.clamp(-1, 1);
+                dbg!(&knot);
             }
-
-            tail_positions.insert(knots[LENGTH - 1]);
+            prior = knot;
         }
+
+        tail_positions.insert(knots[LENGTH - 1]);
     }
 
     tail_positions.len()
